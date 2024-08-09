e03c472a1c99ba87e554928776d9d17f4b12e59e
rosedenna <rosedenna16@gmail.com>
2024-08-09 17:55:12 +0530

    gsap

---
+++ script.js
@@ -0,0 +1,33 @@
+
+  
+
+  gsap.fromTo('.t1', {x:'30rem',opacity : 0},
+      {x:0, opacity:1, ease:'back.out(1.7)',delay : 0.9}
+  )
+  gsap.fromTo('.hood', {x:'30rem',opacity : 0},
+      {x:0, opacity:1, ease:'back.out(1.7)',delay : 1.5}
+  )
+  gsap.fromTo('.t3', {x:'30rem',opacity : 0},
+      {x:0, opacity:1, ease:'back.out(1.7)',delay : 1.9}
+  )
+  gsap.fromTo('.btn1', {x:'30rem',opacity : 0},
+      {x:0, opacity:1, ease:'back.out(1.7)',delay : 1.9}
+  )
+  gsap.fromTo('.main', {y:'30rem',opacity : 0},
+      {y:0, opacity:1, ease:'back.out(1.7)',delay : 1.9}
+  )
+  gsap.fromTo('.head0', {x:'30rem',opacity : 0},
+      {x:0, opacity:1, ease:'back.out(1.7)',delay : 2.9}
+  )
+  gsap.fromTo('.col-3', {y:'30rem',opacity : 0},
+      {y:0, opacity:1, ease:'back.out(1.7)',delay : 2.9}
+  )
+  gsap.fromTo('.col-4', {y:'30rem',opacity : 0},
+      {y:0, opacity:1, ease:'back.out(1.7)',delay : 3.9}
+  )
+  gsap.fromTo('.head1', {x:'30rem',opacity : 0},
+      {x:0, opacity:1, ease:'back.out(1.7)',delay : 3.9}
+  )
+  gsap.fromTo('.head2', {x:'30rem',opacity : 0},
+      {x:0, opacity:1, ease:'back.out(1.7)',delay : 4.7}
+  )
